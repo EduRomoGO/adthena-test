1698816354fe23d84940eb35aca28394cfbe310d
EduRomoGO <eduromogo@outlook.es>
2020-02-23 21:37:18 +0000

    refactor: remove not needed tests

--- src/__tests__/app.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from "enzyme";
-import App from '../app';
-
-it('renders correctly', () => {
-  const rendered = render(<App/>)
-  expect(rendered).toMatchSnapshot();
-})
