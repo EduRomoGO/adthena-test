63a429d1aeb5871e2f04f18222f10981bf71d0ba
EduRomoGO <eduromogo@outlook.es>
2020-02-23 23:33:29 +0000

    test: green

--- src/components/Todos/Todos.jsx
+++ src/components/Todos/Todos.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
 /* eslint-disable */
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 const Todos = () => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const users = await axios.get('https://jsonplaceholder.typicode.com/users');
+      setUsers(users);
+    }
+
+    fetchData();
+  }, [])
+
   return <section>
     <header><h1>Todos App</h1></header>
   </section>;
--- src/components/Todos/Todos.test.jsx
+++ src/components/Todos/Todos.test.jsx
@@ -1,11 +1,41 @@
+/* eslint-disable */
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import Todos from './Todos.jsx';
+import axios from 'axios';
+
+jest.mock('axios');
+
+afterEach(() => {
+  cleanup();
+  jest.resetAllMocks();
+});
 
 
 describe('Todos', () => {
   it('should render correctly', () => {
+    const users = [{
+      "id": 1,
+      "name": "Leanne Graham",
+      "username": "Bret",
+      "email": "Sincere@april.biz",
+      "phone": "1-770-736-8031 x56442",
+      "website": "hildegard.org",
+    },
+    {
+      "id": 2,
+      "name": "Alice",
+      "username": "liz",
+      "email": "liz@april.biz",
+      "phone": "1-770-756-8031 x56442",
+      "website": "liz.com",
+    }];
+
+    axios.get.mockResolvedValueOnce(users);
+
     const { getByRole } = render(<Todos />);
+
+    expect(axios.get).toHaveBeenCalledTimes(1);
 
     expect(getByRole('heading').textContent).toBe('Todos App');
   });
