359df08b163e6e8686a20eeeab5ee2ccf2a21d2e
EduRomoGO <eduromogo@outlook.es>
2020-02-24 00:23:21 +0000

    test: green

--- src/components/Todos/Todos.jsx
+++ src/components/Todos/Todos.jsx
@@ -1,24 +1,38 @@
 /* eslint-disable */
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const Todos = () => {
   const [users, setUsers] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
   useEffect(() => {
     const fetchData = async () => {
-      setIsLoading(true);
-      const users = await axios.get('https://jsonplaceholder.typicode.com/users');
-      setIsLoading(false);
-      setUsers(users);
+      try {
+
+        setIsLoading(true);
+        const users = await axios.get('https://jsonplaceholder.typicode.com/users');
+        setIsLoading(false);
+        setUsers(users);
+      } catch (err) {
+        setIsLoading(false);
+        setIsError(true);
+      }
     }
 
     fetchData();
-  }, [])
+  }, []);
+
+  const renderError = () => {
+    return <div>Something went wrong...</div>;
+  }
 
   return <section>
     <header><h1>Todos App</h1></header>
-    { isLoading ? <div data-testid='loader'>Loading...</div> : ''}
+    {isLoading
+      ? <div data-testid='loader'>Loading...</div>
+      : isError ? renderError() : ''
+    }
   </section>;
 }
 
