cee7fb9e940a6c105d0245bbf3bb66f18d4bdd17
EduRomoGO <eduromogo@outlook.es>
2020-02-25 21:48:49 +0000

    feat: fetch todos

--- src/components/Todos/Todos.jsx
+++ src/components/Todos/Todos.jsx
@@ -1,19 +1,23 @@
 /* eslint-disable */
 import React from 'react';
 import { useFetch } from './useFetch.js';
-import { USERS_URL } from '../../utils/constants.js'
+import { USERS_URL, TODOS_URL } from '../../utils/constants.js'
 
 const Todos = () => {
-  const [users, isLoading, isError] = useFetch(USERS_URL);
+  const [users, isLoadingUsers, isErrorUsers] = useFetch(USERS_URL);
+  const [todos, isLoadingTodos, isErrorTodos] = useFetch(TODOS_URL);
   const renderError = () => {
     return <div>Something went wrong...</div>;
   }
 
+  const isFetchingError = () => isErrorUsers || isErrorTodos;
+  const isLoading = () => isLoadingUsers || isLoadingTodos;
+
   return <section>
     <header><h1>Todos App</h1></header>
-    {isLoading
+    {isLoading()
       ? <div data-testid='loader'>Loading...</div>
-      : isError ? renderError() : ''
+      : isFetchingError() ? renderError() : ''
     }
   </section>;
 }
