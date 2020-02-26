047e5216e716cec1b669bef4302479b3f26c63c0
EduRomoGO <eduromogo@outlook.es>
2020-02-26 23:33:24 +0000

    feat: manage data conversion

--- src/components/Todos/Todos.jsx
+++ src/components/Todos/Todos.jsx
@@ -1,13 +1,37 @@
+import React, { useEffect, useState } from 'react';
 /* eslint-disable */
-import React from 'react';
 import { useFetch } from './useFetch.js';
 import { USERS_URL, TODOS_URL } from '../../utils/constants.js'
 
 const Todos = () => {
   const [users, isLoadingUsers, isErrorUsers] = useFetch(USERS_URL);
   const [todos, isLoadingTodos, isErrorTodos] = useFetch(TODOS_URL);
+  const [completeUsers, setCompleteUsers] = useState([]);
+  useEffect(() => {
+    const getCompleteUsers = (userList, todoList) => {
+      return userList.map(({id, username, email, website}) => ({id, username, email, website}));
+    }
+
+    const convertData = (users, todos) => {
+      if (Object.keys(users).length > 0 && Object.keys(todos).length) {
+        const userList = users.data;
+        const todoList = todos.data;
+
+        setCompleteUsers(getCompleteUsers(userList, todoList));
+      }
+    }
+
+    convertData(users, todos);
+
+  }, [users, todos]);
   const renderError = () => {
     return <div>Something went wrong...</div>;
+  }
+
+  const renderUsers = (users) => {
+    return users ? users.map(({id, username, email, website}) => {
+      return <div key={id}>{username}</div>;
+    }) : '';
   }
 
   const isFetchingError = () => isErrorUsers || isErrorTodos;
@@ -17,7 +41,7 @@ const Todos = () => {
     <header><h1>Todos App</h1></header>
     {isLoading()
       ? <div data-testid='loader'>Loading...</div>
-      : isFetchingError() ? renderError() : ''
+      : isFetchingError() ? renderError() : renderUsers(completeUsers)
     }
   </section>;
 }
